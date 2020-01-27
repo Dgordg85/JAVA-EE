373a338cee175a2e8d6ced8ec424d48e42760faf
Alexandr <Dgordg85@users.noreply.github.com>
2020-01-28 01:33:01 +0300

    Lesson03 (#4)
    * LessonWork
    
    * Delete Lesson Work
    
    * ДЗ 02
    
    * Lesson 03
    
    * Refactor
    
    * Считывание из базы продуктов
    
    * Update ProductRepository.java
    
    * Исправление ошибки
    
    * Update ProductRepository.java
    
    * Исправление ошибок
    
    * Update AppListener.java
    
    * Правки
    
    * Правки jsp
    
    * Refactor
    
    * CRUDE

---
+++ base-webapp/src/main/java/ru/geekbrains/controller/Controller.java
@@ -0,0 +1,36 @@
+package ru.geekbrains.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Controller", urlPatterns = {"", "main", "product", "cart", "order", "company"})
+public class Controller extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html");
+
+        String url = "/WEB-INF/";
+        String path = req.getServletPath();
+
+        switch (path){
+            case "/":
+            case "/main": url += "index.jsp";
+                break;
+            case "/product": url += "product.jsp";
+                break;
+            case "/cart": url += "cart.jsp";
+                break;
+            case "/order": url += "order.jsp";
+                break;
+            case "/company": url += "company.jsp";
+
+        }
+
+        req.getRequestDispatcher(url).forward(req, resp);
+    }
+}
